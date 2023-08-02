234f6c626c7c79916f0025722a8cec768e6a1c2e
Shahbaz Alam <shahbazalamjobs@gmail.com>
2023-08-02 12:39:58 +0530

    Update 01-Javascript-basics.js

--- 05-Introduction to Javascript/01-Javascript-basics.js
+++ 05-Introduction to Javascript/01-Javascript-basics.js
@@ -301,8 +301,26 @@ else {
 
 /* ---------------- */
 
+/* BMI calculator using if-else
+- BMI <18.5, the output should be: "Your BMI is <bmi>, so you are underweight."
+- BMI 18.5-24.9, the output should be "Your BMI is <bmi>, so you have a normal weight.
+- BMI >24.9, the output should be "Your BMI is <bmi>, so you are overweight."
+*/
 
-
+function bmiCalculator (weight, height) {
+    var bmi = weight / (height * height) ;
+    var interpretation = 0;
+    if(bmi < 18.5){
+         interpretation =  "Your BMI is " + bmi + ", so you are underweight.";   
+    }
+    else if(bmi >= 18.5 && bmi <= 24.9){
+         interpretation =  "Your BMI is " + bmi + ", so you have a normal weight.";   
+    }
+    else {
+         interpretation =  "Your BMI is " + bmi + ", so you are overweight.";   
+    }
+    return interpretation;
+}
 
 
 /* ---------------- */
