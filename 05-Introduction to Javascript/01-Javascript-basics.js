e487c7bb522d604b7389219889d8111f618820cd
Shahbaz Alam <shahbazalamjobs@gmail.com>
2023-08-03 09:39:53 +0530

    Update 01-Javascript-basics.js

--- 05-Introduction to Javascript/01-Javascript-basics.js
+++ 05-Introduction to Javascript/01-Javascript-basics.js
@@ -488,12 +488,28 @@ while (i > 0) {
 
 }
 
-
-
-
-
-
 /* ---------------- */
 
+// Using for loop
 
+var output = [];
+
+function fizzBuzz() {
+
+    for(var num = 1; num <= 100; num++) {
+
+        if (num % 3 === 0 && num % 5 === 0) {
+            output.push("FizzBuzz");
+        } else if (num % 3 === 0) {
+            output.push("Fizz");
+        } else if (num % 5 === 0) {
+            output.push("Buzz");
+        } else {
+            output.push(num);
+        }    
+    }
+    console.log(output);        
+}
+
+fizzBuzz();
 
