0e9c52c11c386871be994d3342f65b6039356b4e
Shahbaz Alam <shahbazalamjobs@gmail.com>
2023-08-08 12:11:38 +0530

    Update 01-high-order-function-calculator.js

--- 05-Introduction to Javascript/04-Advance Javascript and DOM/02-Basics/01-high-order-function-calculator.js
+++ 05-Introduction to Javascript/04-Advance Javascript and DOM/02-Basics/01-high-order-function-calculator.js
@@ -24,3 +24,11 @@ function mul (n1, n2) {
 function div (n1, n2) {
     return n1 / n2;
 }
+
+/*
+    add(2,7);   o/p: 9
+    sub(10,4);  o/p : 6
+    mul(10,4);  o/p : 40
+    sub(10,4);  o/p : 2.5
+    div(10,2);  o/p : 5    
+*/
