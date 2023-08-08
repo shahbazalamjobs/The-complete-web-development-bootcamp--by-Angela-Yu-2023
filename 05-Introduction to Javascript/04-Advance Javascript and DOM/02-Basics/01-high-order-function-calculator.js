0ddc8acfa79f7b5944f55da432ea4bfa377be48b
Shahbaz Alam <shahbazalamjobs@gmail.com>
2023-08-08 12:08:51 +0530

    Create 01-high-order-function-calculator.js

---
+++ 05-Introduction to Javascript/04-Advance Javascript and DOM/02-Basics/01-high-order-function-calculator.js
@@ -0,0 +1,26 @@
+
+function calculator (n1, n2, op) {
+    op(n1, n2);
+}
+
+function add (n1, n2) {
+    return n1 + n2;
+}
+
+function sub (n1, n2) {
+    return n1 - n2;
+}
+
+
+function add (n1, n2) {
+    return n1 + n2;
+}
+
+
+function mul (n1, n2) {
+    return n1 * n2;
+}
+
+function div (n1, n2) {
+    return n1 / n2;
+}
