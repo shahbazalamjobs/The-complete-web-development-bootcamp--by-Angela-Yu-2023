efd92e3493a4a8ec14892fd5d08c60555cab3bb0
Shahbaz Alam <shahbazalamjobs@gmail.com>
2023-08-10 20:00:31 +0530

    Update 01-basics.js

--- 06-JQuery/01-basics.js
+++ 06-JQuery/01-basics.js
@@ -3,7 +3,9 @@
 $("h1").css("color", "red");  
 // Selects h1 elements then changes their color to red
 
+
 /* *********** */
+
 
 $(document).ready(function () {
     $("h1").css("color", "red");
@@ -13,7 +15,6 @@ $(document).ready(function () {
 $(document).ready(function() { ... });: 
 This part wraps the code inside a function that will be executed once the document is fully loaded and ready. 
 It ensures that the code runs after the HTML document's elements have been loaded into the browser's memory.
-It can be used when jquery and javascript location are present in head tag 
 */
 
 /*
@@ -21,4 +22,10 @@ It can be used when jquery and javascript location are present in head tag
 2. Targets all `<h1>` elements.
 3. Changes their text color to red using CSS.
 4. Uses jQuery library for easier HTML manipulation.
+*/
+
+/* 
+$(document).ready(function() { ... });: 
+It can be used when jquery and javascript location are present in head tag 
+Or if dont want to use ready(func(){...}); , then better place jquery and javascript location tags before body tag
 */
