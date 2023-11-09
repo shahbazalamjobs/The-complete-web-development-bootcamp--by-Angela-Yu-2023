9c6c02d799ea95cf2a3487ff7219398fe1e8828e
Shahbaz Alam <shahbazalamjobs@gmail.com>
2023-11-09 20:16:31 +0530

    Create index.js

---
+++ 11-EJS/02-Exercise/index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const app = express();
+const port = 3000;
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+let week; // Declare week variable
+
+function weekEnjoyOrWork(req, res, next) {
+    let date = new Date();
+    let day = date.getDay();
+
+    if (day >= 0 && day < 5) {
+        week = "It's a weekday, it's time to work hard!";
+    } else if (day >= 5 && day <= 6) {
+        week = "It's a weekend, it's time to have fun!";
+    }
+    next();
+}
+
+app.use(weekEnjoyOrWork);
+
+app.get('/', (req, res) => {
+    res.render(__dirname + '/views/index.ejs', {
+        weekValue: week
+    });
+});
+
+app.listen(port, () => {
+    console.log(`http://localhost:${port}`);
+});
